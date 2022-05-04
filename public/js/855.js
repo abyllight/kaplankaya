81e40eeda7c6d1a7fb5ec7a746048d13b0ee80fa
Abylay <abyllight@gmail.com>
2022-05-04 13:48:30 +0600

    first commit

---
+++ public/js/855.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[855],{3855:(e,t,n)=>{n.r(t),n.d(t,{default:()=>l});const l=(0,n(1900).Z)({},(function(){var e=this,t=e.$createElement;e._self._c;return e._m(0)}),[function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",[n("h3",[e._v("404. Страница не найдена")])])}],!1,null,null,null).exports}}]);
